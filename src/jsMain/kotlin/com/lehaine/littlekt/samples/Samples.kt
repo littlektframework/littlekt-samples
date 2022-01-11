b078113947f6787f01ba14a5a1e63fb216b415d2
LeHaine <colt.daily@gmail.com>
2022-01-10 21:09:39 -0500

    samples: remove runBlocking and CouroutineScope from JVM and JS targets

--- src/jsMain/kotlin/com/lehaine/littlekt/samples/Samples.kt
+++ src/jsMain/kotlin/com/lehaine/littlekt/samples/Samples.kt
@@ -1,22 +1,15 @@
 package com.lehaine.littlekt.samples
 
 import com.lehaine.littlekt.createLittleKtApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 
 /**
  * @author Colton Daily
  * @date 12/27/2021
  */
 fun main() {
-    val job = Job()
-    val scope = CoroutineScope(job)
-    scope.launch {
-        createLittleKtApp {
-            title = "LittleKt - Samples"
-        }.start {
-            SampleGame(it)
-        }
+    createLittleKtApp {
+        title = "LittleKt - Samples"
+    }.start {
+        SampleGame(it)
     }
 }
--- src/jvmMain/kotlin/com/lehaine/littlekt/samples/Samples.kt
+++ src/jvmMain/kotlin/com/lehaine/littlekt/samples/Samples.kt
@@ -1,21 +1,18 @@
 package com.lehaine.littlekt.samples
 
 import com.lehaine.littlekt.createLittleKtApp
-import kotlinx.coroutines.runBlocking
 
 /**
  * @author Colton Daily
  * @date 12/24/2021
  */
 fun main(args: Array<String>) {
-    runBlocking {
-        createLittleKtApp {
-            width = 960
-            height = 540
-            vSync = true
-            title = "LittleKt - Samples"
-        }.start {
-            SampleGame(it)
-        }
+    createLittleKtApp {
+        width = 960
+        height = 540
+        vSync = true
+        title = "LittleKt - Samples"
+    }.start {
+        SampleGame(it)
     }
 }
